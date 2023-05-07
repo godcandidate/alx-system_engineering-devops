ff51c5b2fae4d4036aa147c32d226e9a6b6d378b
godcandidate <godcandidate@gmail.com>
2023-05-07 21:18:36 +0000

    update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,29 @@
 # Script to install nginx using puppet
 
+# checks if the nginx is present
 package {'nginx':
   ensure => 'present',
 }
 
+# install nginx
 exec {'install':
   command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
 
 }
 
+# echo prints hello 
 exec {'Hello':
   command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
   provider => shell,
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+# redirection
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/www.youtube.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
 
+# restart nginx
 exec {'run':
   command  => 'sudo service nginx restart',
   provider => shell,
