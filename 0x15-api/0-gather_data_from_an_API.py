8e237139dfa9f8e7bb5fd040d832d9c577087c35
godcandidate <godcandidate@gmail.com>
2023-05-22 18:56:13 +0000

    update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-""" a Python script that, using this REST API, for a given employee ID """
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    Id = int(sys.argv[1])
-    url = "https://jsonplaceholder.typicode.com"
-
-    user_url = "{}/users/{}".format(url, Id)
-    user_info = requests.get(user_url).json()
-    name = user_info.get("name")
-
-    tasks_url = "{}/todos".format(url)
-    tasks = requests.get(tasks_url).json()
-    user_t = [obj for obj in tasks if obj.get("userId") == Id]
-    tasks_d = [obj for obj in user_t if obj.get("completed")]
-    us_t = len(user_t)
-    t_done = len(tasks_d)
-
-    print("Employee {} is done with tasks({}/{}):".format(name, t_done, us_t))
-    for task in tasks_d:
-        print("\t {}".format(task.get("title")))
+    url = "https://jsonplaceholder.typicode.com/"
+    name = requests.get("{}users/{}".format(url, sys.argv[1])).json()
+    tasks = requests.get("{}users/{}/todos".format(url, sys.argv[1])).json()
+    c_tasks = [task for task in tasks if task.get("completed") is True]
+    titles = [task.get("title") for task in tasks
+              if task.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name.get("name"), len(c_tasks), len(tasks)))
+    for title in titles:
+        print("\t {}".format(title))
