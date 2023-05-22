05536e14ca902c5cb35a85c02a4923c8e88f9620
godcandidate <godcandidate@gmail.com>
2023-05-22 21:13:43 +0000

    create 2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,32 @@
+""" Python script that export data in the JSON format"""
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    Id = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com"
+
+    user_url = "{}/users/{}".format(url, Id)
+    user_info = requests.get(user_url).json()
+    username = user_info.get("username")
+
+    tasks_url = "{}/todos".format(url)
+    tasks = requests.get(tasks_url).json()
+    user_tasks = [obj for obj in tasks if obj.get("userId") == Id]
+
+    output = {
+        Id: [
+            {
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": username
+            }
+            for task in user_tasks
+        ]
+    }
+
+    filename = "{}.json".format(Id)
+    with open(filename, 'w', encoding="utf-8") as file:
+        json.dump(output, file)
